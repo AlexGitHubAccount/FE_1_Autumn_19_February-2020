22404fc647404d8110b0caae9bf0c3caba168b7e
Aleksander Strukov <mailboxforsasha@gmail.com>
2020-03-05 21:37:47 +0200

    First commit

---
+++ js/shopping-bag.js
@@ -0,0 +1,27 @@
+//Function for increment amount of quantity of items inside the bag
+(function () {
+
+    function countChange(event) {
+        let target = event && event.target || event.srcElement;
+        let btn = target.getAttribute('data-count');
+
+        if (!btn) return;
+
+        if (btn === "plus") {
+            target.previousElementSibling.innerHTML++;
+        }
+        if (btn === "minus") {
+            target.nextElementSibling.innerHTML === "1" ? target.nextElementSibling.innerText = 1 : target.nextElementSibling.innerHTML--;
+        }
+    }
+
+    function initCount() {
+        const count = document.querySelectorAll('.count');
+
+        for (let i = 0; i < count.length; i++) {
+            count[i].addEventListener('click', countChange);
+        }
+    }
+
+   setTimeout(function () { initCount(); }, 0);
+})();
